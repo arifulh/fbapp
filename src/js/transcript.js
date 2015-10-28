53e91899f7b6cb4865c6f28ec418077ac41d7106
arifulh <arifulh@gmail.com>
2015-10-28 03:35:20 -0400

    test data
    moving this out to a mock jsonp call

---
+++ src/js/transcript.js
@@ -0,0 +1,30 @@
+/**
+ * Test json callback
+ */
+function transcriptTestCallback() {
+  return [{
+    t: 2,
+    l: "Bush: Sir, I wil1l now inject him with deadly poison"
+  }, {
+    t: 15,
+    l: "Bush: This man here is my best friend"
+  }, {
+    t: 4,
+    l: "Bush: Sir, they should really drag us into the street and shoot us."
+  }, {
+    t: 6,
+    l: "Ashcroft: Why don't you do something else with your life."
+  }, {
+    t: 1,
+    l: "Bush: Sir, I can only drink Karachi milkshakes."
+  }, {
+    t: 8,
+    l: "Bush: I have one friend, he hates me"
+  }, {
+    t: 11,
+    l: "2Bush: My friend enjoys magicians"
+  }, {
+    t: 36,
+    l: "Bush: We massage eachothers arms."
+  }];
+}
